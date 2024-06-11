f5ed0655506a52285ea5e3c6fafb0c49850534d5
unknown <dantebian95@gmail.com>
2024-06-11 19:41:47 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Tienda Virtual</title>
+        <link rel="stylesheet" href="assets/css/styles.css" />
+    </head>
+
+    <body>
+        <!-- CABECERA -->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/logo.jpg" alt="Tienda Logo" />
+                <a href="index.php">
+                    Tienda de ropa  
+                </a>
+            </div>
+        </header>
+
+        <!-- MENU -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 5</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- BARRA LATERAL -->
+
+        <!-- CONTENIDO CENTRAL -->
+
+        <!-- PIE DE PAGINA -->
+
+    </body>
+</html>
