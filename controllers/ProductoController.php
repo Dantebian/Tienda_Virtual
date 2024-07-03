5ee9abe39e48e1c686f34595bbc6ef29e64e44bd
unknown <dantebian95@gmail.com>
2024-07-03 19:06:00 -0300

    Finish Product details

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -6,9 +6,24 @@ class ProductoController
 {
     public function index()
     {
+        $producto = new Producto();
+        $productos = $producto->getRandom(6);
 
         //Renderizar vista
         require_once 'views/producto/destacados.php';
+    }
+
+    public function ver()
+    {
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+
+            $producto = new Producto();
+            $producto->setId($id);
+
+            $product = $producto->getOne();
+        }
+        require_once 'views/producto/ver.php';
     }
 
     public function gestion()
@@ -67,7 +82,7 @@ class ProductoController
                 if (isset($_GET['id'])) {
                     $id = $_GET['id'];
                     $producto->setId($id);
-                    
+
                     $save = $producto->edit();
                 } else {
                     $save = $producto->save();
--- views/producto/destacados.php
+++ views/producto/destacados.php
@@ -1,22 +1,17 @@
-<h1>Productos destacados</h1>
+<h1>Nuestros productos</h1>
 
-<div class="product">
-    <img src="assets/img/remeras.jpeg" />
-    <h2>Remeras Oversize</h2>
-    <p>$4500</p>
-    <a href="" class="button">Comprar</a>
-</div>
+<?php while ($product = $productos->fetch_object()) : ?>
 
-<div class="product">
-    <img src="assets/img/remeras.jpeg" />
-    <h2>Remeras Oversize</h2>
-    <p>$4500</p>
-    <a href="" class="button">Comprar</a>
-</div>
-
-<div class="product">
-    <img src="assets/img/remeras.jpeg" />
-    <h2>Remeras Oversize</h2>
-    <p>$4500</p>
-    <a href="" class="button">Comprar</a>
-</div>
+    <div class="product">
+        <a href="<?=base_url?>producto/ver&id=<?=$product->id?>">
+            <?php if ($product->imagen != null) : ?>
+                <img src="<?= base_url ?>uploads/images/<?= $product->imagen ?>" />
+            <?php else : ?>
+                <img src="<?= base_url ?>assets/img/remeras.jpeg" />
+            <?php endif; ?>
+            <h2><?= $product->nombre ?></h2>
+        </a>
+        <p><?= '$' . $product->precio ?></p>
+        <a href="" class="button">Comprar</a>
+    </div>
+<?php endwhile; ?>
